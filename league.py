ac7d80501e01f3ac7dc47091fc0dc594facf7b45
Robert Alexander <rla@navadrag.com>
2021-06-12 12:34:17 -0700

    load chris, testing pipeline as a whole from nothing

--- league.py
+++ league.py
@@ -1,29 +1,9 @@
 from league_data import *
 from league_functions import *
 from league_constants import *
-import Game
-import sys
-data = load_fetch_data(robb)
 
-# Aram_Data = fetch_new_data(read_set, Aram_Data, robb)
-d = defaultdict(int)
-for i in data:
-    d[type(i.summoner.stats).__name__] += 1
+data = load_fetch_data(chris)
 
-print(d)
-
-for i in data:
-    if type(i.summoner.stats).__name__ == "ParticipantStats":
-        i.summoner.stats = Game.Stats(i.summoner.stats)
-
-d = defaultdict(int)
-for i in data:
-    d[type(i.summoner.stats).__name__] += 1
-
-print(d)
-
-"""dill.dump(data, file = open("cache/data_dirty_doughnut.pickle", "wb"))
-"""
 # print(read_set)
 # win_rate(robb, Aram_Data)
 # champ_winrate(robb, Aram_Data)
